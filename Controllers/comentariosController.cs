8890195198b51a0b7fcbaa091eb4ab42bef33fbf
Juan Orellana <juan.orellana2@catolica.edu.sv>
2025-02-18 17:28:04 -0600

    Modelos calificaciones y usuario y Controlador Comentario

---
+++ Controllers/comentariosController.cs
@@ -0,0 +1,18 @@
+﻿using L01_NUMEROS_CARNETS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L01_NUMEROS_CARNETS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class comentariosController : ControllerBase
+    {
+        private readonly blogDBContext _blogDBContext;
+
+        public comentariosController(blogDBContext blogDBContext) 
+        {
+            _blogDBContext = blogDBContext;        
+        }
+    }
+}
