f331f04a644845f94fbfa9f8fb3081530bca9ec6
Rafael Sandoval <rafael.sandoval@catolica.edu.sv>
2025-02-18 17:30:37 -0600

    creacion del controlador de publicaciones

---
+++ Controllers/publicacionesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L01_NUMEROS_CARNETS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class publicacionesController : ControllerBase
+    {
+    }
+}
