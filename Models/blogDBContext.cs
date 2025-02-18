aecd17079c0d64bb0d69286564318647a9590cb6
Juan Orellana <juan.orellana2@catolica.edu.sv>
2025-02-18 17:10:10 -0600

    Agregar archivos de proyecto.

---
+++ Models/blogDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L01_NUMEROS_CARNETS.Models
+{
+    public class blogDBContext : DbContext
+    {
+        public blogDBContext(DbContextOptions<blogDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
