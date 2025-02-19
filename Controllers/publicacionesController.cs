c1a4c6ab5af1b0e289b9862334aeabcff0431c1f
Rafael Sandoval <rafael.sandoval@catolica.edu.sv>
2025-02-18 19:04:19 -0600

    creacion de usuariosController con sus metodos y agregar metodos al publicacionesController

--- Controllers/publicacionesController.cs
+++ Controllers/publicacionesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using L01_NUMEROS_CARNETS.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace L01_NUMEROS_CARNETS.Controllers
 {
@@ -7,5 +9,108 @@ namespace L01_NUMEROS_CARNETS.Controllers
     [ApiController]
     public class publicacionesController : ControllerBase
     {
+        private readonly blogDBContext _blogDBContext;
+
+        public publicacionesController(blogDBContext publicacionesContexto)
+        {
+            _blogDBContext = publicacionesContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<publicaciones> listadoPublicaciones = (from p in _blogDBContext.publicaciones
+                                              select p).ToList();
+
+            if (listadoPublicaciones.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoPublicaciones);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarPublicacion([FromBody] publicaciones publicacion)
+        {
+            try
+            {
+                _blogDBContext.publicaciones.Add(publicacion);
+                _blogDBContext.SaveChanges();
+                return Ok(publicacion);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public IActionResult ActualizarPublicacion(int id, [FromBody] publicaciones publicacionModificar)
+        {
+            var publicacionActual = (from p in _blogDBContext.publicaciones
+                                 where p.publicacionId == id
+                                 select p).FirstOrDefault();
+
+            if (publicacionActual == null)
+            {
+                return NotFound();
+            }
+
+            publicacionActual.titulo = publicacionModificar.titulo;
+            publicacionActual.descripcion = publicacionModificar.descripcion;
+            publicacionActual.usuarioId = publicacionModificar.usuarioId;
+
+            _blogDBContext.Entry(publicacionActual).State = EntityState.Modified;
+            _blogDBContext.SaveChanges();
+
+            return Ok(publicacionModificar);
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult EliminarPublicacion(int id)
+        {
+            var publicacion = (from p in _blogDBContext.publicaciones
+                           where p.publicacionId == id
+                           select p).FirstOrDefault();
+
+            if (publicacion == null)
+            {
+                return NotFound();
+            }
+
+            _blogDBContext.publicaciones.Attach(publicacion);
+            _blogDBContext.publicaciones.Remove(publicacion);
+            _blogDBContext.SaveChanges();
+
+            return Ok(publicacion);
+        }
+
+        [HttpGet]
+        [Route("PublicacionesFiltradasPorUsuario")]
+        public IActionResult PublicacionesFiltradasPorUsuario(string usuario)
+        {
+            var publicaciones = (from p in _blogDBContext.publicaciones
+                            join u in _blogDBContext.usuarios on p.publicacionId equals u.usuarioId
+                            where u.nombre == usuario
+                            select new
+                            {
+                                u.usuarioId,
+                                u.nombre,
+                                p.titulo
+                            }).ToList();
+
+            if (publicaciones.Count == 0)
+            {
+                return NotFound($"No se encontraron libros con el título '{usuario}'.");
+            }
+
+            return Ok(publicaciones);
+        }
+
     }
 }
---
+++ Controllers/usuariosController.cs
@@ -0,0 +1,143 @@
+﻿using L01_NUMEROS_CARNETS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01_NUMEROS_CARNETS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usuariosController : ControllerBase
+    {
+        private readonly blogDBContext _blogDBContext;
+
+        public usuariosController(blogDBContext usuariosContexto)
+        {
+            _blogDBContext = usuariosContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<usuarios> listadoUsuarios = (from u in _blogDBContext.usuarios
+                                          select u).ToList();
+
+            if (listadoUsuarios.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoUsuarios);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarUsuario([FromBody] usuarios usuario)
+        {
+            try
+            {
+                _blogDBContext.usuarios.Add(usuario);
+                _blogDBContext.SaveChanges();
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public IActionResult ActualizarUsuario(int id, [FromBody] usuarios usuarioModificar)
+        {
+            var usuarioActual = (from u in _blogDBContext.usuarios
+                               where u.usuarioId == id
+                               select u).FirstOrDefault();
+
+            if (usuarioActual == null)
+            {
+                return NotFound();
+            }
+
+            usuarioActual.rolId = usuarioModificar.rolId;
+            usuarioActual.nombreUsuario = usuarioModificar.nombreUsuario;
+            usuarioActual.clave = usuarioModificar.clave;
+            usuarioActual.nombre = usuarioModificar.nombre;
+            usuarioActual.apellido = usuarioModificar.apellido;
+
+            _blogDBContext.Entry(usuarioActual).State = EntityState.Modified;
+            _blogDBContext.SaveChanges();
+
+            return Ok(usuarioModificar);
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult EliminarUsuario(int id)
+        {
+            var usuario = (from u in _blogDBContext.usuarios
+                         where u.usuarioId == id
+                         select u).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _blogDBContext.usuarios.Attach(usuario);
+            _blogDBContext.usuarios.Remove(usuario);
+            _blogDBContext.SaveChanges();
+
+            return Ok(usuario);
+        }
+
+        [HttpGet]
+        [Route("GetUsuariosFiltrado")]
+        public IActionResult GetUsuariosFiltrados(string nombre = null, string apellido= null)
+        {
+            var usuario = from u in _blogDBContext.usuarios
+                           select u;
+
+            if (!string.IsNullOrEmpty(nombre)) 
+            {
+                usuario = usuario.Where(u => u.nombre.Contains(nombre));
+            }
+            if (!string.IsNullOrEmpty(apellido))
+            {
+                usuario = usuario.Where(u => u.apellido.Contains(apellido));
+            }
+
+            var usuariofiltrado = usuario.ToList();
+
+            if (usuariofiltrado.Count == 0)
+            { 
+                return NotFound("No se encontraron usuarios con los filtros proporcionados.");
+            }
+
+            return Ok(usuariofiltrado);
+        }
+
+        [HttpGet]
+        [Route("BuscarUsuarioPorRol")]
+        public IActionResult BuscarUsuarioPorRol(string rol)
+        {
+            var usuarios = (from u in _blogDBContext.usuarios
+                            join r in _blogDBContext.roles on u.usuarioId equals r.rolId
+                            where r.rol == rol
+                            select new 
+                            {
+                                u.usuarioId,
+                                u.nombreUsuario,
+                                r.rol
+                            }).ToList();
+
+            if (usuarios.Count == 0)
+            {
+                return NotFound($"No se encontraron libros con el título '{rol}'.");
+            }
+
+            return Ok(usuarios);
+        }
+    }
+}
